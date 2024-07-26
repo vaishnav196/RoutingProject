c046826493f63d0d8cdcdf5360a1c91653a29362
vaishnav196 <vaish00721@gmail.com>
2024-07-26 22:21:54 +0530

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { Form1Component } from './form1/form1.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { FormBuilderComponent } from './form-builder/form-builder.component';
 import { APIComponent } from './api/api.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'/home',pathMatch:'full'},
@@ -17,6 +18,7 @@ export const routes: Routes = [
 {path:'form2',component:ReactiveFormComponent},
 {path:'form3',component:FormBuilderComponent},
 {path:'Api',component:APIComponent},
+{path:'Login',component:LoginComponent},
 //{path:'**',component:NotFoundComponent}
 
 
